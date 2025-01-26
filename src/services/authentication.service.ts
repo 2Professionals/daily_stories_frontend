43748722e758bd01b2a239c972329885fdbe820b
Akram <akrambenaoun1993@gmail.com>
2025-01-26 19:16:57 +0100

    auth globally working! still some minor details to be improved

---
+++ src/services/authentication.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private apiUrl = 'http://localhost:3000/api/users/login'; // Update with your login endpoint
+
+  constructor(private http: HttpClient) {}
+
+  login(credentials: { user_email: string; user_password: string }): Observable<any> {
+    return this.http.post<any>(this.apiUrl, credentials);
+  }
+
+  saveToken(token: string): void {
+    localStorage.setItem('authToken', token);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem('authToken');
+  }
+
+  logout(): void {
+    localStorage.removeItem('authToken');
+  }
+}
