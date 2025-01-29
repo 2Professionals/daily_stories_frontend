cdf9554a5ff98bed1a71253349fd480d4c701dfe
Akram <akrambenaoun1993@gmail.com>
2025-01-29 23:48:18 +0100

    comm removed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import { LandingPageComponent } from '../components/landing-page/landing-page.component';
 import { SignupComponent } from '../components/signup/signup.component';
 import { MainFeedComponent } from '../components/main-feed/main-feed.component';
+import { AuthGuard } from '../guards/authentication.guards';
+import { AccessDeniedComponent } from '../components/access-denied/access-denied.component';
 
 export const routes: Routes = [
   { path: '', component: LandingPageComponent },
   { path: 'sign-up', component: SignupComponent },
-  { path: 'main-feed', component: MainFeedComponent },
+  { path: 'main-feed', component: MainFeedComponent, canActivate: [AuthGuard] },
+  { path: 'access-denied', component: AccessDeniedComponent },
+  { path: '**', redirectTo: 'access-denied' },
 ];
