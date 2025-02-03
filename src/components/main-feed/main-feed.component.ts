367f339cda7ba184aaf999aacbe7f9b0d71c52a7
Akram <akrambenaoun1993@gmail.com>
2025-02-03 21:03:00 +0100

    user profile form data

--- src/components/main-feed/main-feed.component.ts
+++ src/components/main-feed/main-feed.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/authentication.service';
-import { RouterLink, Router} from '@angular/router';
+import { RouterLink, Router } from '@angular/router';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { CommonModule } from '@angular/common';
 
@@ -13,12 +13,17 @@ import { CommonModule } from '@angular/common';
 })
 export class MainFeedComponent {
   isLoading: boolean = false;
-  current_year:number = new Date().getFullYear()
-  constructor(
-    private authService: AuthService,
-    private router: Router
-  ) {}
+  current_year: number = new Date().getFullYear();
+  username: String = '';
 
+  constructor(private authService: AuthService, private router: Router) {}
+  ngOnInit() {
+    const userDataRaw = sessionStorage.getItem('userData');
+    if (userDataRaw) {
+      const userData = JSON.parse(userDataRaw);
+      this.username = userData.user_username;
+    }
+  }
   logOut() {
     this.isLoading = true;
     this.authService.logout();
