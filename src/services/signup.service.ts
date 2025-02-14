4cc3f519fb4888db41dae8d3d6eabaf6b091c6d9
Akram <akrambenaoun1993@gmail.com>
2025-02-14 07:48:32 +0100

    add environments

--- src/services/signup.service.ts
+++ src/services/signup.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SignUpService {
-  private apiUrl = 'http://localhost:3000/api/users/addNewUser';
+  private apiUrl = environment.backendURL;
 
   constructor(private http: HttpClient) {}
 
   registerUser(userData: any): Observable<any> {
-    return this.http.post<any>(this.apiUrl, userData);
+    return this.http.post<any>(`${this.apiUrl}/users/addNewUser`, userData);
   }
 }
